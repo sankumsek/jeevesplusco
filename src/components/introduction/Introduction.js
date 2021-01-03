88ac0b203bfa39e1569ce29fba541d2f0430ab20
Sanjeev Sekar <sankumsek@gmail.com>
2021-01-03 00:50:15 -0800

    Customizing Introduction message

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -4,13 +4,13 @@ import Scroll from '../common/Scroll';
 const Introduction = () =>
   <section id="intro" className="wrapper style1 fullscreen fade-up">
     <div className="inner">
-      <h1>Hyperspace</h1>
+      <h1>Jeeves + Co.</h1>
       <p>
-        Just another fine responsive site template designed by{' '}
-        <a href="http://html5up.net">HTML5 UP</a>
+      Your technical strategy and consulting partner{' '}
+        {/* <a href="http://html5up.net">HTML5 UP</a>
         <br />
         and released for free under the{' '}
-        <a href="http://html5up.net/license">Creative Commons</a>.
+        <a href="http://html5up.net/license">Creative Commons</a>. */}
       </p>
       <ul className="actions">
         <li>
